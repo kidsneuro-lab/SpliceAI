ccfc7251c75030d332c7630ceec7524f673d7017
h-joshi <hihi.joshi@gmail.com>
2024-11-16 13:24:26 +0000

    Optimised model loading

--- spliceai/__init__.py
+++ spliceai/__init__.py
@@ -1,8 +1,16 @@
 import signal
-from pkg_resources import get_distribution
+import sys
+from importlib.metadata import version, PackageNotFoundError
+from keras.models import load_model
+from pkg_resources import resource_filename
 
-
-signal.signal(signal.SIGINT, lambda x, y: exit(0))
+signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
 
 name = 'spliceai'
-__version__ = get_distribution(name).version
+try:
+    __version__ = version(name)
+except PackageNotFoundError:
+    __version__ = 'unknown'
+
+paths = ('models/spliceai{}.h5'.format(x) for x in range(1, 6))
+MODELS = [load_model(resource_filename('spliceai', x)) for x in paths]
--- tests/test_delta_score.py
+++ tests/test_delta_score.py
@@ -1,46 +1,44 @@
+import os
+import pytest
 from collections import namedtuple
-import unittest
-from pkg_resources import resource_filename
 from spliceai.utils import Annotator, get_delta_scores
-
 
 Record = namedtuple('Record', ['chrom', 'pos', 'ref', 'alts'])
 
+@pytest.fixture(scope='module')
+def ann():
+    fasta_path = os.path.join(os.path.dirname(__file__), 'data', 'test.fa')
+    return Annotator(fasta_path, 'grch37')
 
-class TestDeltaScore(unittest.TestCase):
+@pytest.fixture(scope='module')
+def ann_without_prefix():
+    fasta_without_prefix_path = os.path.join(os.path.dirname(__file__), 'data', 'test_without_prefix.fa')
+    return Annotator(fasta_without_prefix_path, 'grch37')
 
-    @classmethod
-    def setUpClass(cls):
+def test_get_delta_score_acceptor(ann, ann_without_prefix):
+    record = Record('10', 94077, 'A', ['C'])
+    expected_scores = ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193|0.52|0.67|0.98|0.71|0.00|0.00|0.13|0.08']
+    scores = get_delta_scores(record, ann, 500, 0)
+    assert scores == expected_scores
+    scores = get_delta_scores(record, ann_without_prefix, 500, 0)
+    assert scores == expected_scores
 
-        fasta_path = resource_filename(__name__, 'data/test.fa')
-        fasta_without_prefix_path = resource_filename(__name__, 'data/test_without_prefix.fa')
-        cls.ann = Annotator(fasta_path, 'grch37')
-        cls.ann_without_prefix = Annotator(fasta_without_prefix_path, 'grch37')
+    record = Record('chr10', 94077, 'A', ['C'])
+    scores = get_delta_scores(record, ann, 500, 0)
+    assert scores == expected_scores
+    scores = get_delta_scores(record, ann_without_prefix, 500, 0)
+    assert scores == expected_scores
 
-    def test_get_delta_score_acceptor(self):
+def test_get_delta_score_donor(ann, ann_without_prefix):
+    record = Record('10', 94555, 'C', ['T'])
+    expected_scores = ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0|0.00|0.01|0.97|0.79|0.56|0.71|0.99|0.36']
+    scores = get_delta_scores(record, ann, 500, 0)
+    assert scores == expected_scores
+    scores = get_delta_scores(record, ann_without_prefix, 500, 0)
+    assert scores == expected_scores
 
-        record = Record('10', 94077, 'A', ['C'])
-        scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193|0.52|0.67|0.98|0.71|0.00|0.00|0.13|0.08'])
-        scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193|0.52|0.67|0.98|0.71|0.00|0.00|0.13|0.08'])
-
-        record = Record('chr10', 94077, 'A', ['C'])
-        scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193|0.52|0.67|0.98|0.71|0.00|0.00|0.13|0.08'])
-        scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193|0.52|0.67|0.98|0.71|0.00|0.00|0.13|0.08'])
-
-    def test_get_delta_score_donor(self):
-
-        record = Record('10', 94555, 'C', ['T'])
-        scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0|0.00|0.01|0.97|0.79|0.56|0.71|0.99|0.36'])
-        scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0|0.00|0.01|0.97|0.79|0.56|0.71|0.99|0.36'])
-
-        record = Record('chr10', 94555, 'C', ['T'])
-        scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0|0.00|0.01|0.97|0.79|0.56|0.71|0.99|0.36'])
-        scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0|0.00|0.01|0.97|0.79|0.56|0.71|0.99|0.36'])
+    record = Record('chr10', 94555, 'C', ['T'])
+    scores = get_delta_scores(record, ann, 500, 0)
+    assert scores == expected_scores
+    scores = get_delta_scores(record, ann_without_prefix, 500, 0)
+    assert scores == expected_scores
